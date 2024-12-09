ba5430397aa231330f5b21edf97f53adffe75389
ajneelansh <ajneelansh@gmail.com>
2024-12-09 23:39:26 +0530

    Added routes

---
+++ routes/BookstoreRoutes.go
@@ -0,0 +1,15 @@
+package routes
+
+import(
+	"github.com/gorilla/mux"
+	"github.com/ajneelansh/Golang-BMS/controllers"
+
+)
+
+var RsgisterBookRoutes = func(router *mux.Router){
+	router.HandleFunc("/book/",controllers.CreateBooks()).Methods("POST")
+	router.HandleFunc("/book/",controllers.GetBooks()).Methods("GET")
+	router.HandleFunc("/book/{bookId}",controllers.GetBooksById()).Methods("GET")
+	router.HandleFunc("/book/{bookId}",controllers.UpdateBook()).Methods("PUT")
+	router.HandleFunc("/book/{bookId}",controllers.DeketeBook()).Methods("DELETE")
+}
